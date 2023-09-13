fd2f34fcffb269c7b844e103517e17aaeea0f310
Joel Brown <jj.brown042@gmail.com>
2023-09-13 15:31:02 -0500

    ml_model init 2

--- ml_model/model.py
+++ ml_model/model.py
@@ -1,1 +1,12 @@
 #script to train and save model
+
+# Example code to train and save a model
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Train the model
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+
+# Save the model
+joblib.dump(clf, 'model.pkl')
--- ml_model/predict.py
+++ ml_model/predict.py
@@ -1,1 +1,10 @@
 #script to make prediction if audio is a gunshot
+
+# Example code to load a model and make predictions
+import joblib
+
+# Load the model
+clf = joblib.load('model.pkl')
+
+# Make predictions
+predictions = clf.predict(X_new)
